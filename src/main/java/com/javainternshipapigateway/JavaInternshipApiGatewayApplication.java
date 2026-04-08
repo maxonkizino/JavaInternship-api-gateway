11154e80097de9312a3023de0a39f3bc76f72f2f
maxonkizino <mkizino83@gmail.com>
2026-04-08 19:16:14 +0300

    first commit

--- src/main/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplication.java
+++ src/main/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaInternshipApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaInternshipApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternshipApiGatewayApplication.class, args);
+    }
 
 }
