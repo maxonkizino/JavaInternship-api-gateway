3bc5585dff076ae56fbbda1bdd80cb1d8bde117b
maxonkizino <mkizino83@gmail.com>
2026-04-08 19:30:58 +0300

    feat: add gateway configuration

--- src/main/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplication.java
+++ src/main/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.javainternshipapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JavaInternshipApiGatewayApplication {
 
     public static void main(String[] args) {
