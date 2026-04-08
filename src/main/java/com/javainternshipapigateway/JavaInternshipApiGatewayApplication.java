97aff0493d15711e01d1da8bf19d5ef438d968d9
maxonkizino <mkizino83@gmail.com>
2026-04-08 19:16:02 +0300

    first commit

---
+++ src/main/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.javainternshipapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternshipApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInternshipApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javainternshipapigateway/JavaInternshipApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainternshipapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternshipApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
